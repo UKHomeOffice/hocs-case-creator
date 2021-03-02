52a5709103234c9712cece225e6e971877178765
Phil Barton <phil.barton@digital.homeoffice.gov.uk>
2021-03-02 14:21:11 +0000

    HOCS-2542 - skeleton spring boot project

---
+++ src/main/java/uk/gov/digital/ho/hocs/PlaceHolderApplication.java
@@ -0,0 +1,11 @@
+package uk.gov.digital.ho.hocs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlaceHolderApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(PlaceHolderApplication.class, args);
+    }
+}
---
+++ src/test/java/uk/gov/digital/ho/hocs/PlaceHolderApplicationTest.java
@@ -0,0 +1,11 @@
+package uk.gov.digital.ho.hocs;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class PlaceHolderApplicationTest {
+    @Test
+    public void contextLoads() {
+    }
+}
