bc03cc1f0ceb5532fe7ac4512c120125d26bd144
Paul Rau <paul.rau1@digital.homeoffice.gov.uk>
2021-03-22 16:18:31 +0000

    HOCS 2527 Kubernetes Integration Autoscale added. Deployment yaml and script added.

---
+++ deploy.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+set -euo pipefail
+
+export KUBE_NAMESPACE=${ENVIRONMENT}
+export KUBE_SERVER=${KUBE_SERVER}
+export KUBE_TOKEN=${KUBE_TOKEN}
+export VERSION=${VERSION}
+
+if [[ ${KUBE_NAMESPACE} == *prod ]]
+then
+    export MIN_REPLICAS="2"
+    export MAX_REPLICAS="6"
+else
+    export MIN_REPLICAS="1"
+    export MAX_REPLICAS="3"
+fi
+
+cd kd
+
+kd --insecure-skip-tls-verify \
+   --timeout 15m \
+    -f deployment.yaml \
+    -f autoscale.yaml
