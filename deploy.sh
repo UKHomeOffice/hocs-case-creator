a7f50f408468a03f8ae1fe69db1a28062714c738
Liam MacPherson <11508628+LiamMacP@users.noreply.github.com>
2021-03-23 13:51:52 +0000

    HOCS-2527: specify cluster name (#4)
    Currently the pipeline is failing because the `$CLUSTER_NAME` is not 
    specified. This change enforces that this is specified.

--- deploy.sh
+++ deploy.sh
@@ -6,6 +6,17 @@ export KUBE_SERVER=${KUBE_SERVER}
 export KUBE_TOKEN=${KUBE_TOKEN}
 export VERSION=${VERSION}
 
+if [[ ${KUBE_NAMESPACE} == *prod ]]
+then
+    export MIN_REPLICAS="2"
+    export MAX_REPLICAS="6"
+    export CLUSTER_NAME="acp-prod"
+else
+    export MIN_REPLICAS="1"
+    export MAX_REPLICAS="3"
+    export CLUSTER_NAME="acp-notprod"
+fi
+
 export KUBE_CERTIFICATE_AUTHORITY=/tmp/acp.crt
 if ! curl --silent --fail --retry 5 \
 	https://raw.githubusercontent.com/UKHomeOffice/acp-ca/master/$CLUSTER_NAME.crt -o $KUBE_CERTIFICATE_AUTHORITY; then
@@ -13,16 +24,7 @@ if ! curl --silent --fail --retry 5 \
 	exit 1
 fi
 
-if [[ ${KUBE_NAMESPACE} == *prod ]]
-then
-    export MIN_REPLICAS="2"
-    export MAX_REPLICAS="6"
-else
-    export MIN_REPLICAS="1"
-    export MAX_REPLICAS="3"
-fi
-
-cd kd
+cd kd || exit 1
 
 kd --timeout 15m \
     -f deployment.yaml \
