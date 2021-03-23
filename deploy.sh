45bff7228249436dc4db10810cf8c16668f9c09d
Paul Rau <52495258+Paulcr-ho@users.noreply.github.com>
2021-03-23 13:26:08 +0000

    HOCS-2527 Kubernetes Integration (#3)
    Kube certificate authority added due to removal of --insecure-skip-tls-verify

--- deploy.sh
+++ deploy.sh
@@ -6,6 +6,13 @@ export KUBE_SERVER=${KUBE_SERVER}
 export KUBE_TOKEN=${KUBE_TOKEN}
 export VERSION=${VERSION}
 
+export KUBE_CERTIFICATE_AUTHORITY=/tmp/acp.crt
+if ! curl --silent --fail --retry 5 \
+	https://raw.githubusercontent.com/UKHomeOffice/acp-ca/master/$CLUSTER_NAME.crt -o $KUBE_CERTIFICATE_AUTHORITY; then
+	echo "failed to download ca for kube api"
+	exit 1
+fi
+
 if [[ ${KUBE_NAMESPACE} == *prod ]]
 then
     export MIN_REPLICAS="2"
