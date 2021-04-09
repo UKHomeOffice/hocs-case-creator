0f684d0f8245927f23e5646a225729efccf674bd
Phil Barton <phil.barton@digital.homeoffice.gov.uk>
2021-04-09 11:24:16 +0100

    HOCS-2527 : Reinstate localstack hostname as a property.

--- src/main/java/uk/gov/digital/ho/hocs/aws/LocalStackConfiguration.java
+++ src/main/java/uk/gov/digital/ho/hocs/aws/LocalStackConfiguration.java
@@ -12,6 +12,7 @@ import com.amazonaws.services.sns.AmazonSNS;
 import com.amazonaws.services.sns.AmazonSNSClientBuilder;
 import com.amazonaws.services.sqs.AmazonSQS;
 import com.amazonaws.services.sqs.AmazonSQSClientBuilder;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -21,6 +22,9 @@ import org.springframework.context.annotation.Profile;
 public class LocalStackConfiguration {
 
     public static final String REGION = "eu-west-2";
+    @Value("${aws.local-host:localhost}")
+    private String awsHost;
+    
     private final AWSCredentialsProvider awsCredentialsProvider = new AWSCredentialsProvider() {
 
         @Override
@@ -41,7 +45,7 @@ public class LocalStackConfiguration {
 
     @Bean("sqsClient")
     public AmazonSQS sqsClient() {
-        String host = "http://localhost:4576/";
+        String host = String.format("http://%s:4576/", awsHost);
         AwsClientBuilder.EndpointConfiguration endpoint = new AwsClientBuilder.EndpointConfiguration(host, "eu-west-2");
         return AmazonSQSClientBuilder.standard()
                 .withClientConfiguration(new ClientConfiguration().withProtocol(Protocol.HTTP))
@@ -57,7 +61,7 @@ public class LocalStackConfiguration {
 
     @Bean("auditSnsClient")
     public AmazonSNS auditSnsClient() {
-        String host = "http://localhost:4575/";
+        String host = String.format("http://%s:4575/", awsHost);
         AwsClientBuilder.EndpointConfiguration endpoint = new AwsClientBuilder.EndpointConfiguration(host, REGION);
         return AmazonSNSClientBuilder.standard()
                 .withClientConfiguration(new ClientConfiguration().withProtocol(Protocol.HTTP))
@@ -68,7 +72,7 @@ public class LocalStackConfiguration {
 
     @Bean("s3Client")
     public AmazonS3 s3Client() {
-        String host = "http://localhost:4572/";
+        String host = String.format("http://%s:4572/", awsHost);
         AwsClientBuilder.EndpointConfiguration endpoint = new AwsClientBuilder.EndpointConfiguration(host, REGION);
         return AmazonS3ClientBuilder.standard()
                 .withClientConfiguration(new ClientConfiguration().withProtocol(Protocol.HTTP))
