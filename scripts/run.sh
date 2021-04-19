a5ee4e0083b90dbb7a1d06fef5bfddc58ba032ec
Phil Barton <phil.barton@digital.homeoffice.gov.uk>
2021-04-19 17:28:57 +0100

    HOCS-2611 - Catch SIGTERM in shell script

--- scripts/run.sh
+++ scripts/run.sh
@@ -3,4 +3,16 @@
 NAME=${NAME:-case-creator}
 
 JAR=$(find . -name ${NAME}*.jar|head -1)
+
+handleSigTerm() {
+  echo "Caught SIGTERM"
+  kill -TERM "$child" 2>/dev/null
+}
+
+trap handleSigTerm SIGTERM
+
 java ${JAVA_OPTS} -Dcom.sun.management.jmxremote.local.only=false -Djava.security.egd=file:/dev/./urandom -jar "${JAR}"
+
+child=$!
+wait "$child"
+
