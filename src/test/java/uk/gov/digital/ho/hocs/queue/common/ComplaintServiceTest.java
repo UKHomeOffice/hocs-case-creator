2835f680423051571f9782721765a3a178489b3b
Phil Barton <phil.barton@digital.homeoffice.gov.uk>
2021-04-20 13:24:24 +0100

    HOCS-2611 - Swallow exceptions tests

--- src/test/java/uk/gov/digital/ho/hocs/queue/common/ComplaintServiceTest.java
+++ src/test/java/uk/gov/digital/ho/hocs/queue/common/ComplaintServiceTest.java
@@ -25,6 +25,7 @@ import java.time.LocalDate;
 import java.util.List;
 import java.util.UUID;
 
+import static org.junit.Assert.fail;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.*;
@@ -49,9 +50,30 @@ public class ComplaintServiceTest {
     private ComplaintService complaintService;
 
     private String user;
+    private UUID stageForCaseUUID;
+    private UUID primaryCorrespondent;
+    private CreateCaseRequest createCaseRequest;
+    private CreateCaseResponse createCaseResponse;
+    private String json;
+    private String expectedText;
+    private final String s3ObjectName = "8bdc5724-80e4-4fe3-a0a9-1f00262107b0";
+    private UUID caseUUID;
+    private ComplaintTypeData complaintTypeData;
 
     @Before
     public void setUp() {
+        json = getResourceFileAsString("staffBehaviour.json");
+        expectedText = getResourceFileAsString("staffBehaviour.txt");
+
+        LocalDate receivedDate = LocalDate.parse("2020-10-03");
+        String decsReference = "COMP/01";
+        caseUUID = UUID.randomUUID();
+        stageForCaseUUID = UUID.randomUUID();
+        primaryCorrespondent = UUID.randomUUID();
+        complaintTypeData = new UKVITypeData();
+        DocumentSummary documentSummary = new DocumentSummary(ORIGINAL_FILENAME, DOCUMENT_TYPE, s3ObjectName);
+        createCaseRequest = new CreateCaseRequest(complaintTypeData.getCaseType(), receivedDate, List.of(documentSummary));
+        createCaseResponse = new CreateCaseResponse(caseUUID, decsReference);
         user = UUID.randomUUID().toString();
         when(clientContext.getUserId()).thenReturn(user);
         complaintService = new ComplaintService(workflowClient, caseworkClient, clientContext, auditClient, documentS3Client);
@@ -59,27 +81,8 @@ public class ComplaintServiceTest {
 
     @Test
     public void shouldCreateComplaint() throws IOException {
-        String json = getResourceFileAsString("staffBehaviour.json");
-        String expectedText = getResourceFileAsString("staffBehaviour.txt");
 
-        LocalDate receivedDate = LocalDate.parse("2020-10-03");
-        String decsReference = "COMP/01";
-        UUID caseUUID = UUID.randomUUID();
-        UUID stageForCaseUUID = UUID.randomUUID();
-        UUID primaryCorrespondent = UUID.randomUUID();
-        ComplaintTypeData complaintTypeData = new UKVITypeData();
-        String s3ObjectName = "8bdc5724-80e4-4fe3-a0a9-1f00262107b0";
-        DocumentSummary documentSummary = new DocumentSummary(ORIGINAL_FILENAME, DOCUMENT_TYPE, s3ObjectName);
-        CreateCaseRequest createCaseRequest = new CreateCaseRequest(complaintTypeData.getCaseType(), receivedDate, List.of(documentSummary));
-        CreateCaseResponse createCaseResponse = new CreateCaseResponse(caseUUID, decsReference);
-
-        when(documentS3Client.storeUntrustedDocument(ORIGINAL_FILENAME, expectedText)).thenReturn(s3ObjectName);
-
-        when(workflowClient.createCase(createCaseRequest)).thenReturn(createCaseResponse);
-
-        when(caseworkClient.getStageForCase(caseUUID)).thenReturn(stageForCaseUUID);
-
-        when(caseworkClient.getPrimaryCorrespondent(caseUUID)).thenReturn(primaryCorrespondent);
+        goodSetup();
 
         complaintService.createComplaint(new UKVIComplaintData(json), complaintTypeData);
 
@@ -94,4 +97,70 @@ public class ComplaintServiceTest {
         verify(auditClient).audit(eq(EventType.CREATOR_CORRESPONDENT_CREATED), eq(caseUUID), eq(stageForCaseUUID), anyMap());
     }
 
+    private void goodSetup() {
+        when(documentS3Client.storeUntrustedDocument(ORIGINAL_FILENAME, expectedText)).thenReturn(s3ObjectName);
+
+        when(workflowClient.createCase(createCaseRequest)).thenReturn(createCaseResponse);
+
+        when(caseworkClient.getStageForCase(caseUUID)).thenReturn(stageForCaseUUID);
+
+        when(caseworkClient.getPrimaryCorrespondent(caseUUID)).thenReturn(primaryCorrespondent);
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void storeUntrustedDocumentShouldThrowException() {
+        goodSetup();
+        when(documentS3Client.storeUntrustedDocument(ORIGINAL_FILENAME, expectedText)).thenThrow(new NullPointerException());
+        complaintService.createComplaint(new UKVIComplaintData(json), complaintTypeData);
+    }
+    
+    @Test(expected = NullPointerException.class)
+    public void createCaseShouldThrowException() {
+        goodSetup();
+        when(workflowClient.createCase(createCaseRequest)).thenThrow(new NullPointerException());
+        complaintService.createComplaint(new UKVIComplaintData(json), complaintTypeData);
+    }
+
+    @Test
+    public void getStageForCaseShouldCatchException() {
+        goodSetup();
+        when(caseworkClient.getStageForCase(caseUUID)).thenThrow(new NullPointerException());
+        complaintService.createComplaint(new UKVIComplaintData(json), complaintTypeData);
+    }
+    
+    @Test
+    public void auditShouldCatchException() throws IOException {
+        goodSetup();
+        doThrow(IOException.class).when(auditClient).audit(EventType.CREATOR_CASE_CREATED, caseUUID, stageForCaseUUID, json);
+        complaintService.createComplaint(new UKVIComplaintData(json), complaintTypeData);
+    }
+    
+    @Test
+    public void updateStageUserShouldCatchException() {
+        goodSetup();
+        when(caseworkClient.updateStageUser(caseUUID, stageForCaseUUID, UUID.fromString(user))).thenThrow(new NullPointerException());
+        complaintService.createComplaint(new UKVIComplaintData(json), complaintTypeData);
+    }
+    
+    @Test
+    public void addCorrespondentToCaseShouldCatchException() {
+        goodSetup();
+        when(caseworkClient.addCorrespondentToCase(eq(caseUUID), eq(stageForCaseUUID), any(ComplaintCorrespondent.class))).thenThrow(new NullPointerException());
+        complaintService.createComplaint(new UKVIComplaintData(json), complaintTypeData);
+    }
+    
+    @Test
+    public void getPrimaryCorrespondentShouldCatchException() {
+        goodSetup();
+        when(caseworkClient.getPrimaryCorrespondent(caseUUID)).thenThrow(new NullPointerException());
+        complaintService.createComplaint(new UKVIComplaintData(json), complaintTypeData);
+    }
+    
+    @Test
+    public void advanceCaseShouldCatchException() {
+        goodSetup();
+        when(workflowClient.advanceCase(eq(caseUUID), eq(stageForCaseUUID), anyMap())).thenThrow(new NullPointerException());
+        complaintService.createComplaint(new UKVIComplaintData(json), complaintTypeData);
+    }
+    
 }
