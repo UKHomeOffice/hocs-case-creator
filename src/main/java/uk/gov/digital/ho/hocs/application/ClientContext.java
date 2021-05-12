716a12836c634dbe499f46b538432b49c6838144
Phil Barton <phil.barton@digital.homeoffice.gov.uk>
2021-05-12 15:31:51 +0100

    Feature/hocs 2793 add complaint to cch (#25)
    * HOCS-2793 - Change teams to CCH_webform after complaint creation.
    
    * HOCS-2793 - Remove unused context.

--- src/main/java/uk/gov/digital/ho/hocs/application/ClientContext.java
+++ src/main/java/uk/gov/digital/ho/hocs/application/ClientContext.java
@@ -13,15 +13,17 @@ public class ClientContext {
     public static final String CORRELATION_ID_HEADER = "X-Correlation-Id";
     public static final String USER_ID_HEADER = "X-Auth-UserId";
     public static final String GROUP_HEADER = "X-Auth-Groups";
+    public static final String TEAM_ID = "TEAM_ID";
     public static final ThreadLocal<Map<String, String>> THREAD_LOCAL = new ThreadLocal<>();
 
 
-    public void setContext(String userId, String groups, String correlationId) {
+    public void setContext(String userId, String groups, String team, String correlationId) {
         THREAD_LOCAL.remove();
         Map<String, String> context = new HashMap<>();
         context.put(CORRELATION_ID_HEADER, correlationId);
         context.put(USER_ID_HEADER, userId);
         context.put(GROUP_HEADER, groups);
+        context.put(TEAM_ID, team);
         THREAD_LOCAL.set(context);
     }
 
@@ -40,5 +42,8 @@ public class ClientContext {
         return threadMap.get(GROUP_HEADER);
     }
 
+    public String getTeamId() {
+        Map<String, String> threadMap = THREAD_LOCAL.get();
+        return threadMap.get(TEAM_ID);
+    }
 }
-
