69d22c56762acb71376c5f42355bf9745e61fee0
Phil Barton <phil.barton@digital.homeoffice.gov.uk>
2021-05-13 11:05:27 +0100

    HOCS-2793 - Add team to config
    * HOCS-2793 - Move identity config from secret to config map
    
    user, team and group

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # kube-hocs-case-creator
 Kubernetes configuration of the case creator project
+
+## Config Map Values
+
+### CASE_CREATOR_UKVI_COMPLAINT_USER
+The user UUID after creating a "system user" as the identity for UKVI complaint creation.
+
+### CASE_CREATOR_UKVI_COMPLAINT_TEAM
+The team UUID is taken from the `team` table in the info schema. It is the team associated with the display_name: "CCH Webform Cases"
+
+### CASE_CREATOR_UKVI_COMPLAINT_GROUP
+The group after creating a "system user" as the identity for UKVI complaint creation.
--- deploy.sh
+++ deploy.sh
@@ -9,8 +9,14 @@ export VERSION=${VERSION}
 if [[ ${KUBE_NAMESPACE} == *prod ]]
 then
     export CLUSTER_NAME="acp-prod"
+    export CASE_CREATOR_UKVI_COMPLAINT_USER="TBD"
+    export CASE_CREATOR_UKVI_COMPLAINT_TEAM="TBD"
+    export CASE_CREATOR_UKVI_COMPLAINT_GROUP="TBD"
 else
     export CLUSTER_NAME="acp-notprod"
+    export CASE_CREATOR_UKVI_COMPLAINT_USER="22ca514d-c205-47a0-9e38-68daee4c1299"
+    export CASE_CREATOR_UKVI_COMPLAINT_TEAM="08e30ffc-2087-ff3a-b19b-343a88491347"
+    export CASE_CREATOR_UKVI_COMPLAINT_GROUP="/COMP_CCH_zqxmzQ6iEkTRw"
 fi
 
 export KUBE_CERTIFICATE_AUTHORITY="https://raw.githubusercontent.com/UKHomeOffice/acp-ca/master/${CLUSTER_NAME}.crt"
@@ -18,4 +24,5 @@ export KUBE_CERTIFICATE_AUTHORITY="https://raw.githubusercontent.com/UKHomeOffic
 cd kd || exit 1
 
 kd --timeout 15m \
+    -f identityConfigMap.yaml \
     -f deployment.yaml
