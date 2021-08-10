893b876cf775be8931c98575734129c5d82c662d
Phil Barton <phil.barton@digital.homeoffice.gov.uk>
2021-08-10 13:12:12 +0100

    HOCS-3568 - config issue in not prod. (#12)
    Set user id to ukvi-case-creator@homeoffice.gov.uk user

--- deploy.sh
+++ deploy.sh
@@ -15,7 +15,7 @@ then
     export UPTIME_PERIOD="Mon-Sun 05:10-22:50 Europe/London"
 else
     export CLUSTER_NAME="acp-notprod"
-    export CASE_CREATOR_UKVI_COMPLAINT_USER="22ca514d-c205-47a0-9e38-68daee4c1299"
+    export CASE_CREATOR_UKVI_COMPLAINT_USER="96193359-228a-4d22-908a-eb44e9877163" # found in keycloak. the id of ukvi-case-creator@homeoffice.gov.uk
     export CASE_CREATOR_UKVI_COMPLAINT_TEAM="08e30ffc-2087-ff3a-b19b-343a88491347"
     export CASE_CREATOR_UKVI_COMPLAINT_GROUP="/COMP_CCH_zqxmzQ6iEkTRw"
     export UPTIME_PERIOD="Mon-Fri 08:10-17:50 Europe/London"
