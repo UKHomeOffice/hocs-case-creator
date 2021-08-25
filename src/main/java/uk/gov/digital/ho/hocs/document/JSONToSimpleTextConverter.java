9218bc972796469169d869926880501c580c9758
Robert Deniszczyc <robert.deniszczyc@digital.homeoffice.gov.uk>
2021-08-25 09:58:20 +0100

    HOCS-3671 Indents complaint text after a line break

--- src/main/java/uk/gov/digital/ho/hocs/document/JSONToSimpleTextConverter.java
+++ src/main/java/uk/gov/digital/ho/hocs/document/JSONToSimpleTextConverter.java
@@ -55,6 +55,7 @@ public class JSONToSimpleTextConverter {
         } else {
             String textValue = node.textValue();
             if (keyName.equals("complaintText")) {
+                textValue = textValue.replaceAll("[\\n]", NEW_LINE_STR);
                 textValue = WordUtils.wrap(NEW_LINE_STR + textValue, WRAP_LENGTH, NEW_LINE_STR, WRAP_LONG_WORDS);
             }
             convertedOutput.append(String.format("%" + (level * 4 - 3) + "s %s : %s%n", "", fromJavaIdentifierToDisplayableString(keyName), textValue));
