18bacb776a51fe6aa450afd8cd8f4a5029074296
Nick McMahon <nick.mcmahon@digital.homeoffice.gov.uk>
2021-11-02 22:25:47 +0000

    HOCS-4029 reduce the sensitivity of the HPA and update the version of the main sidecar pods (#14)
    * HOCS-4029
    This commit reduces the sensitivity of the HPA and updates the version of the main sidecar pods to latest.
    
    * Feedback from PR, update list of k8s resources to deploy
    
    * Update deployment.yaml
    
    correct indentation

--- deploy.sh
+++ deploy.sh
@@ -8,23 +8,31 @@ export VERSION=${VERSION}
 
 if [[ ${KUBE_NAMESPACE} == *prod ]]
 then
-    export CLUSTER_NAME="acp-prod"
     export CASE_CREATOR_UKVI_COMPLAINT_USER="TBD"
     export CASE_CREATOR_UKVI_COMPLAINT_TEAM="TBD"
     export CASE_CREATOR_UKVI_COMPLAINT_GROUP="TBD"
+
+    export MIN_REPLICAS="1"
+    export MAX_REPLICAS="2"
+
     export UPTIME_PERIOD="Mon-Sun 05:10-22:50 Europe/London"
 else
-    export CLUSTER_NAME="acp-notprod"
-    export CASE_CREATOR_UKVI_COMPLAINT_USER="96193359-228a-4d22-908a-eb44e9877163" # found in keycloak. the id of ukvi-case-creator@homeoffice.gov.uk
+    export CASE_CREATOR_UKVI_COMPLAINT_USER="96193359-228a-4d22-908a-eb44e9877163"
     export CASE_CREATOR_UKVI_COMPLAINT_TEAM="08e30ffc-2087-ff3a-b19b-343a88491347"
     export CASE_CREATOR_UKVI_COMPLAINT_GROUP="/COMP_CCH_zqxmzQ6iEkTRw"
+
+    export MIN_REPLICAS="1"
+    export MAX_REPLICAS="2"
+
     export UPTIME_PERIOD="Mon-Fri 08:10-17:50 Europe/London"
 fi
 
 export KUBE_CERTIFICATE_AUTHORITY="https://raw.githubusercontent.com/UKHomeOffice/acp-ca/master/${CLUSTER_NAME}.crt"
 
-cd kd || exit 1
+cd kd
 
-kd --timeout 15m \
+kd --timeout 10m \
     -f identityConfigMap.yaml \
-    -f deployment.yaml
+    -f deployment.yaml \
+    -f service.yaml \
+    -f autoscale.yaml
