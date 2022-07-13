85405588f1ef4228b626c8c2cd06b008b31e69fe
sathik <sathik.abdulrahman@digital.homeoffice.gov.uk>
2022-07-13 19:09:42 +0100

    HOCS-5148 - Addressed review comments on https://github.com/UKHomeOffice/hocs-case-creator/pull/141

--- src/main/java/uk/gov/digital/ho/hocs/queue/data/CaseData.java
+++ src/main/java/uk/gov/digital/ho/hocs/queue/data/CaseData.java
@@ -19,18 +19,9 @@ import java.util.Optional;
 public abstract class CaseData implements ComplaintData {
 
     static final String CREATION_DATE = "$.creationDate";
-    static final String COMPLAINT_TYPE = "$.complaint.complaintType";
-    static final String APPLICANT_TYPE = "$.complaint.reporterDetails.applicantType";
-    static final String APPLICANT_APPLICANT_NAME = "$.complaint.reporterDetails.applicantName";
-    static final String APPLICANT_APPLICANT_EMAIL = "$.complaint.reporterDetails.applicantEmail";
-    static final String APPLICANT_APPLICANT_PHONE = "$.complaint.reporterDetails.applicantPhone";
-    static final String AGENT_APPLICANT_NAME = "$.complaint.reporterDetails.applicantDetails.applicantName";
-    static final String AGENT_AGENT_NAME = "$.complaint.reporterDetails.agentDetails.agentName";
-    static final String AGENT_AGENT_EMAIL = "$.complaint.reporterDetails.agentDetails.agentEmail";
-    static final String AGENT_AGENT_PHONE = "$.complaint.reporterDetails.agentDetails.agentPhone";
 
-    private final ReadContext ctx;
-    private final String jsonBody;
+    protected final ReadContext ctx;
+    protected final String jsonBody;
 
     public CaseData(String jsonBody) {
         this.jsonBody = jsonBody;
@@ -40,41 +31,6 @@ public abstract class CaseData implements ComplaintData {
     @Override
     public LocalDate getDateReceived() {
         return LocalDate.parse(ctx.read(CREATION_DATE));
-    }
-
-    @Override
-    public String getComplaintType() {
-        return ctx.read(COMPLAINT_TYPE);
-    }
-
-    @Override
-    public List<ComplaintCorrespondent> getComplaintCorrespondent() {
-
-        List<ComplaintCorrespondent> correspondents = new ArrayList<>();
-
-        try {
-            String applicantType = ctx.read(APPLICANT_TYPE);
-            if (applicantType.equals("APPLICANT")) {
-                ComplaintCorrespondent applicantCorrespondent = new ComplaintCorrespondent(ctx.read(APPLICANT_APPLICANT_NAME), CorrespondentType.COMPLAINANT);
-                optionalString(ctx, APPLICANT_APPLICANT_EMAIL).ifPresent(applicantCorrespondent::setEmail);
-                optionalString(ctx, APPLICANT_APPLICANT_PHONE).ifPresent(applicantCorrespondent::setTelephone);
-                correspondents.add(applicantCorrespondent);
-            } else if (applicantType.equals("AGENT")) {
-                ComplaintCorrespondent applicantCorrespondent = new ComplaintCorrespondent(ctx.read(AGENT_APPLICANT_NAME), CorrespondentType.COMPLAINANT);
-                ComplaintCorrespondent agentCorrespondent = new ComplaintCorrespondent(ctx.read(AGENT_AGENT_NAME), CorrespondentType.THIRD_PARTY_REP);
-                optionalString(ctx, AGENT_AGENT_EMAIL).ifPresent(agentCorrespondent::setEmail);
-                optionalString(ctx, AGENT_AGENT_PHONE).ifPresent(agentCorrespondent::setTelephone);
-                //First correspondent added becomes the primary correspondent for the case.
-                correspondents.add(applicantCorrespondent);
-                correspondents.add(agentCorrespondent);
-            } else {
-                throw new IllegalStateException("APPLICANT_TYPE Unknown : " + applicantType);
-            }
-        } catch (PathNotFoundException e) {
-            log.info("getComplaintCorrespondent, no correspondents found for case.");
-        }
-
-        return correspondents;
     }
 
     @Override
@@ -94,7 +50,7 @@ public abstract class CaseData implements ComplaintData {
         return jsonBody;
     }
 
-    private Optional<String> optionalString(ReadContext ctx, String path) {
+    public Optional<String> optionalString(ReadContext ctx, String path) {
         try {
             String value = ctx.read(path);
             return Optional.of(value);
