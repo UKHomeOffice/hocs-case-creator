de5224d25d08a9d5193a60f16f1ab13426671dc8
sathik <sathik.abdulrahman@digital.homeoffice.gov.uk>
2022-07-13 19:14:20 +0100

    HOCS-5148 - Addressed review comments on https://github.com/UKHomeOffice/hocs-case-creator/pull/141

--- src/main/java/uk/gov/digital/ho/hocs/queue/migration/MigrationType.java
+++
@@ -1,9 +0,0 @@
-package uk.gov.digital.ho.hocs.queue.migration;
-
-/**
- * Enum for Migration MessageTypes.
- * May be added with few other types once we got the Migration Data.
- */
-public enum MigrationType {
-    MIGRATION
-}
