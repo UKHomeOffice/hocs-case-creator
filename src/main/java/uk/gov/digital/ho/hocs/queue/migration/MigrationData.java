0addfbff8b9ea4f37adabd453eeb21ca4946a94d
Rishi Ramgolam <rishi.ramgolam@madetech.com>
2022-10-17 15:29:47 +0100

    Case creator changes - to fix json schema next

--- src/main/java/uk/gov/digital/ho/hocs/queue/migration/MigrationData.java
+++ src/main/java/uk/gov/digital/ho/hocs/queue/migration/MigrationData.java
@@ -1,15 +1,9 @@
 package uk.gov.digital.ho.hocs.queue.migration;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import lombok.extern.slf4j.Slf4j;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import uk.gov.digital.ho.hocs.client.casework.dto.ComplaintCorrespondent;
-import uk.gov.digital.ho.hocs.client.migration.casework.dto.MigrationComplaintCorrespondent;
-import uk.gov.digital.ho.hocs.queue.complaints.CorrespondentType;
 import uk.gov.digital.ho.hocs.queue.data.CaseData;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 @Slf4j
@@ -32,10 +26,6 @@ public class MigrationData extends CaseData {
         return null;
     }
 
-    /**
-     *  This assumes only one correspondent per migration message. To verify once we received the data.
-     *  @return a list of correspondent objects
-     */
 
     public String getPrimaryCorrespondent() {
         return ctx.read(PRIMARY_CORRESPONDENT).toString();
