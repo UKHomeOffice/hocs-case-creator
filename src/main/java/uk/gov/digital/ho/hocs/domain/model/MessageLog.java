82a3b99442494d16f47f5d10236254b28acfd34b
Liam MacPherson <liam.macpherson@digital.homeoffice.gov.uk>
2023-02-03 16:26:45 +0000

    Add Repository and Entities

---
+++ src/main/java/uk/gov/digital/ho/hocs/domain/model/MessageLog.java
@@ -0,0 +1,39 @@
+package uk.gov.digital.ho.hocs.domain.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Setter
+@Getter
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name="message_log")
+public class MessageLog {
+
+    @Id
+    @Column(name = "message_id")
+    protected String messageId;
+
+    @Column(name = "external_reference")
+    protected UUID externalReference;
+
+    @Column(name = "message")
+    protected String message;
+
+    @Column(name = "status")
+    @Enumerated(EnumType.STRING)
+    protected Status status;
+
+    @Column(name = "completed")
+    protected LocalDateTime completed;
+
+    @Column(name = "received")
+    protected LocalDateTime received;
+}
---
+++ src/main/java/uk/gov/digital/ho/hocs/service/MessageLogService.java
@@ -0,0 +1,37 @@
+package uk.gov.digital.ho.hocs.service;
+
+import org.springframework.stereotype.Service;
+import uk.gov.digital.ho.hocs.domain.model.MessageLog;
+import uk.gov.digital.ho.hocs.domain.model.Status;
+import uk.gov.digital.ho.hocs.domain.repositories.MessageLogRepository;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Service
+public class MessageLogService {
+
+    private final MessageLogRepository messageLogRepository;
+
+    //Constructor that injects the MessageLogRepository
+    public MessageLogService(MessageLogRepository messageLogRepository) {
+        this.messageLogRepository = messageLogRepository;
+    }
+
+    //Method to create a message log entry
+    public void createMessageLogEntry(String messageId, UUID externalReference, String message) {
+        var messageLog =
+                new MessageLog(messageId, externalReference, message, Status.PENDING, null, LocalDateTime.now());
+        messageLogRepository.save(messageLog);
+    }
+
+    //Method to update a message log entry status
+    public void updateMessageLogEntryStatus(String messageId, Status status) {
+        messageLogRepository.updateStatus(messageId, status);
+    }
+
+    public void completeMessageLogEntry(String messageId) {
+        messageLogRepository.updateStatusAndCompleted(messageId, Status.COMPLETED);
+    }
+
+}
