da6c7d59d5bc2c0764b2bc5efef152738a66ebc2
Liam MacPherson <liam.macpherson@digital.homeoffice.gov.uk>
2023-02-14 11:59:11 +0000

    HOCS-6168: add external reference to listeners
    Add the ability for an external reference to be passed through the Headers of messages on the SQS. This allows for a lookup of callees if required.

--- src/main/java/uk/gov/digital/ho/hocs/queue/common/MigrationQueueListener.java
+++ src/main/java/uk/gov/digital/ho/hocs/queue/common/MigrationQueueListener.java
@@ -7,8 +7,10 @@ import org.springframework.cloud.aws.messaging.listener.annotation.SqsListener;
 import org.springframework.context.annotation.Profile;
 import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.stereotype.Service;
+import uk.gov.digital.ho.hocs.service.MessageLogService;
 
 import java.util.List;
+import java.util.UUID;
 
 @Service
 @Slf4j
@@ -17,27 +19,37 @@ public class MigrationQueueListener {
 
     private final List<MessageHandler> queueMessageHandlers;
 
+    private final MessageLogService messageLogService;
+
     private final boolean shouldIgnoreMessages;
 
     public MigrationQueueListener(List<MessageHandler> queueMessageHandlers,
+                                  MessageLogService messageLogService,
                                   @Value("${aws.sqs.ignore-messages:false}") boolean shouldIgnoreMessages) {
         this.queueMessageHandlers = queueMessageHandlers;
+        this.messageLogService = messageLogService;
         this.shouldIgnoreMessages = shouldIgnoreMessages;
     }
 
     @SqsListener(value = "${aws.sqs.case-migrator.url}", deletionPolicy = SqsMessageDeletionPolicy.NO_REDRIVE)
-    public void onMigrationEvent(String message, @Header("MessageId") String messageId) throws Exception {
+    public void onMigrationEvent(String message,
+                                 @Header("MessageId") String messageId,
+                                 @Header(value = "ExternalReference", required = false) UUID externalReference) throws Exception {
+        // Create message log entry
+        messageLogService.createMessageLogEntry(messageId, externalReference, message);
+
         if (shouldIgnoreMessages) {
             log.warn("Message flagged to ignore: {}", messageId);
-            return;
-        }
-
-        for (MessageHandler messageHandler :
-                queueMessageHandlers) {
-            if (messageHandler.getMessageType().equals(MessageTypes.MIGRATION)) {
-                messageHandler.handleMessage(message, messageId);
-                break;
+        } else {
+            for (MessageHandler messageHandler :
+                    queueMessageHandlers) {
+                if (messageHandler.getMessageType().equals(MessageTypes.MIGRATION)) {
+                    messageHandler.handleMessage(message, messageId);
+                    break;
+                }
             }
         }
+
+        messageLogService.completeMessageLogEntry(messageId);
     }
 }
