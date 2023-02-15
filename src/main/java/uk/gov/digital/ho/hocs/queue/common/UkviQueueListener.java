57c30032e6697c1de2376e7c276170b2bd72db67
Liam MacPherson <liam.macpherson@digital.homeoffice.gov.uk>
2023-02-15 13:08:59 +0000

    HOCS-6168: remove multiple handlers code
    This change enforces that only a specific handler can be brought in for each listener class, simplifying the logic. This change also includes the addition of an optional MessageType header in the Listener. This should be used to ensure that the right listener is called per message. If the wrong type is specified, an exception is thrown and the respective message is flagged with an appropriate status.

--- src/main/java/uk/gov/digital/ho/hocs/queue/common/UkviQueueListener.java
+++ src/main/java/uk/gov/digital/ho/hocs/queue/common/UkviQueueListener.java
@@ -7,9 +7,12 @@ import org.springframework.cloud.aws.messaging.listener.annotation.SqsListener;
 import org.springframework.context.annotation.Profile;
 import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.stereotype.Service;
+import uk.gov.digital.ho.hocs.application.LogEvent;
+import uk.gov.digital.ho.hocs.domain.exceptions.ApplicationExceptions;
+import uk.gov.digital.ho.hocs.domain.model.Status;
+import uk.gov.digital.ho.hocs.queue.complaints.ukvi.UKVIComplaintMessageHandler;
 import uk.gov.digital.ho.hocs.service.MessageLogService;
 
-import java.util.List;
 import java.util.UUID;
 
 @Service
@@ -17,17 +20,16 @@ import java.util.UUID;
 @Profile("ukvi")
 public class UkviQueueListener {
 
-    // Contains a single queue message handler for now
-    private final List<MessageHandler> queueMessageHandlers;
+    private final MessageHandler messageHandler;
 
     private final MessageLogService messageLogService;
 
     private final boolean shouldIgnoreMessages;
 
-    public UkviQueueListener(List<MessageHandler> queueMessageHandlers,
+    public UkviQueueListener(UKVIComplaintMessageHandler messageHandler,
                              MessageLogService messageLogService,
                              @Value("${aws.sqs.ignore-messages:false}") boolean shouldIgnoreMessages) {
-        this.queueMessageHandlers = queueMessageHandlers;
+        this.messageHandler = messageHandler;
         this.messageLogService = messageLogService;
         this.shouldIgnoreMessages = shouldIgnoreMessages;
     }
@@ -35,21 +37,23 @@ public class UkviQueueListener {
     @SqsListener(value = "${aws.sqs.case-creator.url}", deletionPolicy = SqsMessageDeletionPolicy.ON_SUCCESS)
     public void onComplaintEvent(String message,
                                  @Header("MessageId") String messageId,
+                                 @Header(value = "MessageType", required = false) MessageType messageType,
                                  @Header(value = "ExternalReference", required = false) UUID externalReference) throws Exception {
         // Create message log entry
         messageLogService.createMessageLogEntry(messageId, externalReference, message);
 
         if (shouldIgnoreMessages) {
             log.warn("Message flagged to ignore: {}", messageId);
+            messageLogService.updateMessageLogEntryStatus(messageId, Status.IGNORED);
+            return;
+        }
+
+        if (messageType == null ||
+                messageHandler.getMessageType().equals(messageType)) {
+            messageHandler.handleMessage(messageId, message);
         } else {
-            for (MessageHandler messageHandler :
-                    queueMessageHandlers) {
-                // Handles the only message at the minute, should be adapted to read a property from the message.
-                if (messageHandler.getMessageType().equals(MessageTypes.UKVI_COMPLAINTS)) {
-                    messageHandler.handleMessage(messageId, message);
-                    break;
-                }
-            }
+            messageLogService.updateMessageLogEntryStatus(messageId, Status.MESSAGE_TYPE_INVALID);
+            throw new ApplicationExceptions.InvalidMessageTypeException(String.format("Invalid message type %s", messageType), LogEvent.INVALID_MESSAGE_TYPE);
         }
 
         messageLogService.completeMessageLogEntry(messageId);
--- src/test/java/uk/gov/digital/ho/hocs/queue/common/UkviQueueListenerTest.java
+++ src/test/java/uk/gov/digital/ho/hocs/queue/common/UkviQueueListenerTest.java
@@ -9,9 +9,19 @@ import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.util.ReflectionTestUtils;
+import uk.gov.digital.ho.hocs.domain.exceptions.ApplicationExceptions;
+import uk.gov.digital.ho.hocs.domain.model.Status;
 import uk.gov.digital.ho.hocs.queue.complaints.ukvi.UKVIComplaintMessageHandler;
+import uk.gov.digital.ho.hocs.service.MessageLogService;
 
-import static org.mockito.Mockito.*;
+import java.util.UUID;
+
+import static org.mockito.Mockito.any;
+import static org.mockito.Mockito.eq;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
+import static org.mockito.Mockito.when;
 import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
 
 @SpringBootTest(webEnvironment = RANDOM_PORT)
@@ -22,28 +32,56 @@ public class UkviQueueListenerTest {
     @MockBean
     private UKVIComplaintMessageHandler ukviComplaintMessageHandler;
 
+    @MockBean
+    private MessageLogService messageLogService;
+
     @Autowired
     private UkviQueueListener ukviQueueListener;
 
     @Test
-    public void messageTypeDoesNotMatch_doNothing() throws Exception {
-        when(ukviComplaintMessageHandler.getMessageType()).thenReturn(MessageTypes.UNKNOWN);
+    public void messageTypeMatches_callHandler() throws Exception {
+        when(ukviComplaintMessageHandler.getMessageType()).thenCallRealMethod();
 
-        ukviQueueListener.onComplaintEvent("test", "test", null);
+        ukviQueueListener.onComplaintEvent("test", "test", MessageType.UKVI_COMPLAINTS, UUID.randomUUID());
 
+        verify(messageLogService).createMessageLogEntry(eq("test"), any(UUID.class), eq("test"));
         verify(ukviComplaintMessageHandler).getMessageType();
+        verify(ukviComplaintMessageHandler).handleMessage("test", "test");
+        verify(messageLogService).completeMessageLogEntry(eq("test"));
         verifyNoMoreInteractions(ukviComplaintMessageHandler);
     }
 
+    @Test
+    public void messageTypeNull_callHandler() throws Exception {
+        ukviQueueListener.onComplaintEvent("test", "test", null, UUID.randomUUID());
+
+        verify(messageLogService).createMessageLogEntry(eq("test"), any(UUID.class), eq("test"));
+        verify(ukviComplaintMessageHandler, times(0)).getMessageType();
+        verify(ukviComplaintMessageHandler).handleMessage("test", "test");
+        verify(messageLogService).completeMessageLogEntry(eq("test"));
+        verifyNoMoreInteractions(ukviComplaintMessageHandler);
+    }
+
+    @Test(expected = ApplicationExceptions.InvalidMessageTypeException.class)
+    public void messageTypeNotMatch_callHandler() throws Exception {
+        when(ukviComplaintMessageHandler.getMessageType()).thenCallRealMethod();
+
+        ukviQueueListener.onComplaintEvent("test", "test", MessageType.MIGRATION, UUID.randomUUID());
+
+        verify(messageLogService).createMessageLogEntry(eq("test"), any(UUID.class), eq("test"));
+        verify(ukviComplaintMessageHandler).getMessageType();
+        verify(messageLogService).updateMessageLogEntryStatus(eq("test"), eq(Status.MESSAGE_TYPE_INVALID));
+        verifyNoMoreInteractions(ukviComplaintMessageHandler);
+    }
 
     @Test
     @DirtiesContext(methodMode = DirtiesContext.MethodMode.AFTER_METHOD)
     public void whenMessageShouldBeIgnored_doNothing() throws Exception {
         ReflectionTestUtils.setField(ukviQueueListener, "shouldIgnoreMessages", true);
 
-        when(ukviComplaintMessageHandler.getMessageType()).thenReturn(MessageTypes.UKVI_COMPLAINTS);
+        when(ukviComplaintMessageHandler.getMessageType()).thenReturn(MessageType.MIGRATION);
 
-        ukviQueueListener.onComplaintEvent("test", "test", null);
+        ukviQueueListener.onComplaintEvent("test", "test", null, null);
 
         verifyNoMoreInteractions(ukviComplaintMessageHandler);
     }
